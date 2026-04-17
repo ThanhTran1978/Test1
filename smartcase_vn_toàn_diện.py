3d0487c5b6331d4560e5ad0c6a228b29d45e723c
ThanhTran1978 <68649013+ThanhTran1978@users.noreply.github.com>
2026-04-17 13:12:21 +0700

    smartcase_vn.py

--- smartcase_vn_toàn_diện.py
+++ smartcase_vn_toàn_diện.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1PSdcPvCUIwWfTPIFbDM-6plvqmaUfbOr
 """
 
-!pip install streamlit
+pip install streamlit
 
 import streamlit as st
 import pandas as pd
